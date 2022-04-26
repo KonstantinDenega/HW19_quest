e9deb72d5faed61fb43051587c8657f6dc248423
Konstantin Denega <vicond87@gmail.com>
2022-04-26 15:57:55 +0700

    Создание интерфеса и подключение его к классам

---
+++ HW19_quest/Business/Unknown.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW19_quest.Business
+{
+    internal class Unknown: ICreature
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long IdtypeCreature { get; set; }
+        public string SkinCovers { get; set; }
+        public string Skeleton { get; set; }
+        public string MuscularSystem { get; set; }
+        public string RespiratorySystem { get; set; }
+        public string BloodSystem { get; set; }
+        public string NervousSystem { get; set; }
+    }
+}
